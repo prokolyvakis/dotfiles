bbfa6fa408a2c5e43e76057f2a2f7750d3213319
Prodromos Kolyvakis <prokolyvakis@gmail.com>
2021-05-09 12:35:54 +0300

    added jinja2 templates for bashrc and zshrc

---
+++ files/bash_scripts/aux.sh
@@ -0,0 +1,5 @@
+# For compilers to find libffi you may need to set:
+export LDFLAGS="-L/usr/local/opt/libffi/lib"
+
+# For pkg-config to find libffi you may need to set:
+export PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig"
