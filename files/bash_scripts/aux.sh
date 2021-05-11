a11e519572a71680b1302828c395a1ce641057cd
Prodromos Kolyvakis <prokolyvakis@gmail.com>
2021-05-11 14:01:23 +0300

    Update aux.sh

--- files/bash_scripts/aux.sh
+++ files/bash_scripts/aux.sh
@@ -1,5 +1,14 @@
-# For compilers to find libffi you may need to set:
-export LDFLAGS="-L/usr/local/opt/libffi/lib"
-
 # For pkg-config to find libffi you may need to set:
 export PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig"
+
+# For compilers to find libffi you may need to set:
+export LDFLAGS="-L/usr/local/opt/zlib/lib -L/usr/local/opt/bzip2/lib -L/usr/local/opt/libffi/lib"
+export CPPFLAGS="-I/usr/local/opt/zlib/include -I/usr/local/opt/bzip2/include"
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+eval "$(thefuck --alias)"
+eval "$(fasd --init auto)"
